d223e2492127b26ac995afd2263ef6eb9f9fd93b
ambika-devi <ambikadevithota5@gmail.com>
2022-12-23 16:52:07 +0530

    making chat app real time

--- frontend/chatWindow.js
+++ frontend/chatWindow.js
@@ -4,6 +4,8 @@ const textBox = document.querySelector("#input-msgbox");
 const toast = document.querySelector(".toast-msg");
 axios.defaults.headers.common["Authorization"] =localStorage.getItem("usertoken");
 
+
+//creating toast messages
 const createToast = (msg, color = "orangered") => {
   // console.log("toast creted");
   const div = document.createElement("div");
@@ -16,15 +18,19 @@ const createToast = (msg, color = "orangered") => {
   setTimeout(() => {
     // console.log("toast removed");
     div.remove();
-  }, 5000);
+  }, 2000);
 };
 
+
+//adding messages
 const addToChatCard = (name, text) => {
     const html = `<li class="list-group-item">${name}: ${text}</li>`;
     chatMessages.insertAdjacentHTML("beforeend", html);
     textBox.value = "";
   };
 
+
+  //sending single message
 const sendMsg = async (e) => {
   //e.preventDefault();
   const text = textBox.value;
@@ -41,6 +47,7 @@ const sendMsg = async (e) => {
      console.log(response);
     if (response.status == 200) {
         addToChatCard(response.data.userName, text);
+        text.value="";
       createToast(response.data.msg, "green");
     }
   } catch (error) {
@@ -53,23 +60,35 @@ const sendMsg = async (e) => {
   }
 };
 
-const onPageLoaded = async () => {
+
+//to get all chats
+const getAllChats = async () => {
     try {
+      chatMessages.innerHTML="";
       const response = await axios.get("http://localhost:3000/chat/allchats");
       if (response.status == 200) {
         const chats = response.data.chats;
         chats.forEach((chat) => {
           addToChatCard(response.data.userName, chat.chatMessage);
         });
-        createToast(response.data.msg, "green");
+        //createToast(response.data.msg, "green");
       }
     } catch (error) {
       console.log(error);
       if (error.response.status == 400) {
         createToast(error.response.data.msg);
+      }else if(error.response.status==401){
+        createToast(error.response.data.msg);
       }
     }
   };
+
+  //page refreshing after every one second
+  const onPageLoaded=()=>{
+    setInterval(()=>{
+      getAllChats();
+    },5000);
+  }
 
 sendBtn.addEventListener("click", sendMsg);
 document.addEventListener("DOMContentLoaded",onPageLoaded);
